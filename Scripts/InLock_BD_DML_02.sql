7b7a6a468956b1ff92d9102945ad0658565d72b3
Renansalvino <renansalvinomartins454@gmail.com>
2020-10-11 17:00:25 -0300

    para formatar

--- Scripts/InLock_BD_DML_02.sql
+++ Scripts/InLock_BD_DML_02.sql
@@ -12,3 +12,4 @@ VALUES ('Blizzard'),('Rockstar Studios'),('Square Enix')
 INSERT INTO Jogo (NomeJogo,Descricao,DataLancamento,Valor,IdEstudio)
 VALUES ('Diablo 3','É um jogo que contém bastante ação e é viciante, seja você um novato ou um fã','15/05/2012','R$ 99,00',1),('Red Dead Redemption II','jogo eletrônico de Ação-aventura western','26/10/2018','R$ 120,00',2)
 
+SELECT*FROM TipoUsuario
